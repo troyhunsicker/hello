e5980ff7b5454a16b800266165ca651e5c2cd374
troyhunsicker <thunsicker2@huskers.unl.edu>
2022-08-25 19:21:02 +0000

    added my major to the output

---
+++ hello.c
@@ -0,0 +1,19 @@
+/**
+ * Author: Troy Hunsicker!!!!!
+ *         thunsicker2@huskers.unl.edu
+ * Date: 2022/08/23
+ * Major: Electrical Engineering
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Hello World!\n");
+  printf("Troy Hunsicker\n");
+
+  return 0;
+}
