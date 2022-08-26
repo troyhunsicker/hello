cda18278372a1a83cfd0e763a16cd4b026c54fd1
DSchawang <dschawang3@huskers.unl.edu>
2022-08-26 01:53:39 +0000

    Derek added his Major and name to the output

--- hello.c
+++ hello.c
@@ -15,6 +15,8 @@ int main(int argc, char **argv) {
   printf("Hello World!\n");
   printf("Troy Hunsicker!!!\n");
   printf("Electrical Engineering\n);
+  printf("Derek Schawang\n");
+  printf("Electrical Engineering\n");
 
   return 0;
 }
