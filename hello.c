d6505e8fa3488759f8972ee001aae9f3215724df
troyhunsicker <111887435+troyhunsicker@users.noreply.github.com>
2022-08-25 21:35:49 -0500

    Update hello.c

--- hello.c
+++ hello.c
@@ -14,7 +14,7 @@ int main(int argc, char **argv) {
 
   printf("Hello World!\n");
   printf("Troy Hunsicker!!!\n");
-  printf("Electrical Engineering\n);
+  printf("Electrical Engineering\n");
   printf("Derek Schawang\n");
   printf("Electrical Engineering\n");
 
